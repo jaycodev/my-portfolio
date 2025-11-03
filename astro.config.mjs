e721d74cd3d704c8d791eb218af7bf1d55a9d8e8
jaycodev <jason.vilac@gmail.com>
2025-11-03 16:23:58 -0500

    build(vercel): add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
 
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
